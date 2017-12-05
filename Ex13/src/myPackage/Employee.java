721178ba2014eedaffa6ac8ff7f7b7dc8b179eda
parkjaeseong <tmakdlfwotjd@naver.com>
2017-12-05 11:13:26 +0900

    ex13_1

---
+++ Ex13/src/myPackage/Employee.java
@@ -0,0 +1,36 @@
+package myPackage;
+
+public class Employee {
+	private int id;
+	private String name;
+
+	public Employee() {
+	}
+
+	public Employee(int id, String name) {
+		setId(id);
+		setName(name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printInfo()
+	{
+		System.out.println("id : " + getId() + " name : " + getName());
+	}
+
+}
---
+++ Ex13/src/myPackage/mainEmployee.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+
+public class mainEmployee {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
+		Employee employee1 = (Employee) context.getBean("employee1");
+		printInfo(employee1);
+		Employee employee2 = (Employee) context.getBean("employee2");
+		printInfo(employee2);
+	}
+	
+	public static void printInfo(Employee employee)
+	{
+		System.out.printf("ID : %d, Name : %s ",employee.getId(),employee.getName());
+	}
+}
